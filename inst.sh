c8226274fc4c1dd27ffd3abae52fb92f837072e5
Khiemproducer <khiem26032005@gmail.com>
2024-08-25 10:29:07 +0700

    s

--- inst.sh
+++ inst.sh
@@ -1,12 +1,12 @@
 clear
-echo "Đang cài đặt"
+echo "Đang cài đặt SNI 0đ"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/v111.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Khiemproducer/Viettel-SNI/raw/master/vpnznetwork.zip && unzip v111.zip && chmod a+x neko && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/t1 && mv t1 motd
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/Khiemproducer/Viettel-SNI/raw/master/conf && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/Khiemproducer/Viettel-SNI/raw/master/read && chmod +x * && mv l1 login &&
 clear
-echo "Đã cài đặt thành công"
+echo "Đã cài đặt thành công SNI 0đ"
 login
